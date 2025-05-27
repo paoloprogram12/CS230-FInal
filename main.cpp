1dd3b85342f5d5c07ea31e59e48d594f03477a71
paoloprogram12 <sethpsalazar@gmail.com>
2025-05-26 23:07:46 -0700

    vertex and edge class

--- main.cpp
+++ main.cpp
@@ -1,1 +1,30 @@
+#include <iostream>
+using namespace std;
 
+class Vertex {
+private:
+    int num;
+    string name;
+
+public:
+    Vertex(int num, string name) : num(num), name(name) {}
+};
+
+class Edge {
+private:
+    int source;
+    int destination;
+    int weight;
+
+public:
+    Edge(int source, int destination, int weight) : source(source), destination(destination), weight(weight) {}
+
+    // compare weight with other weight
+    bool operator<(const Edge& other) const { return this->weight < other.weight; }
+};
+
+int main() {
+
+
+    return 0;
+}

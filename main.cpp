5997d4171e2ae7ef8b2a5adf4f96446d35645e57
paoloprogram12 <sethpsalazar@gmail.com>
2025-05-28 13:34:15 -0700

    some def

--- main.cpp
+++ main.cpp
@@ -23,6 +23,17 @@ public:
     bool operator<(const Edge& other) const { return this->weight < other.weight; }
 };
 
+//kruskal's algorithm is an algorithm that is used to find the minimum spanning tree of a graph.
+// how it works:
+// sorts all the edges in non-decreasing order by weight
+// initalize a set of trees where each vertex is its own separate tree
+// go through each edge in sorted order, if the edge connects two different trees, then it adds it to the MST
+
+// Minimum Spanning Tree (MST)
+// connects all vertices so there are no isolated nodes
+// has no cycles
+// has the minimum possible total edge weight
+
 int main() {
 
 
